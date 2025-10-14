17028e1dfd9f063514a77607bc995ae481f5e04e
rockeet <rockeet@gmail.com>
2025-10-15 00:09:13 +0800

    rank_select_se_256: select[01]_upper_bound_line(_safe)?: remove redundant block & indent

--- src/terark/succinct/rank_select_se_256.cpp
+++ src/terark/succinct/rank_select_se_256.cpp
@@ -230,19 +230,17 @@ const noexcept {
         else
             hi = mid;
     }
-    {
-        __m256i vec0 = _mm256_add_epi32(_mm256_set1_epi32(lo), _mm256_set_epi32(7,6,5,4,3,2,1,0));
-        vec0 = _mm256_sllv_epi32(vec0, _mm256_set1_epi32(LineShift));
-        __mmask8 k = _bzhi_u32(-1, veclen);
-        __m512i vec1 = _mm512_maskz_loadu_epi64(k, &rank_cache[lo]);
-        __m256i vec2 = _mm512_cvtepi64_epi32(vec1); // keep lev1(low32)
-        __m256i vec3 = _mm256_sub_epi32(vec0, vec2);
-        __m256i key = _mm256_set1_epi32(Rank0);
-        __mmask8 cmp = _mm256_mask_cmpgt_epi32_mask(k, vec3, key);
-        auto tz = _tzcnt_u32(cmp | (1u << veclen)); // upper bound
-        lo += tz;
-        TERARK_ASSERT_LT(Rank0, LineBits * lo - rank_cache[lo].lev1);
-    }
+    __m256i vec0 = _mm256_add_epi32(_mm256_set1_epi32(lo), _mm256_set_epi32(7,6,5,4,3,2,1,0));
+    vec0 = _mm256_sllv_epi32(vec0, _mm256_set1_epi32(LineShift));
+    __mmask8 k = _bzhi_u32(-1, veclen);
+    __m512i vec1 = _mm512_maskz_loadu_epi64(k, &rank_cache[lo]);
+    __m256i vec2 = _mm512_cvtepi64_epi32(vec1); // keep lev1(low32)
+    __m256i vec3 = _mm256_sub_epi32(vec0, vec2);
+    __m256i key = _mm256_set1_epi32(Rank0);
+    __mmask8 cmp = _mm256_mask_cmpgt_epi32_mask(k, vec3, key);
+    auto tz = _tzcnt_u32(cmp | (1u << veclen)); // upper bound
+    lo += tz;
+    TERARK_ASSERT_LT(Rank0, LineBits * lo - rank_cache[lo].lev1);
   #else
     while (lo < hi) {
         size_t mid = (lo + hi) / 2;
@@ -321,16 +319,14 @@ const noexcept {
         else
             hi = mid;
     }
-    {
-        __mmask8 k = _bzhi_u32(-1, veclen);
-        __m512i vec1 = _mm512_maskz_loadu_epi64(k, &rank_cache[lo]);
-        __m256i vec2 = _mm512_cvtepi64_epi32(vec1); // keep lev1(low32)
-        __m256i key = _mm256_set1_epi32(Rank1);
-        __mmask8 cmp = _mm256_mask_cmpgt_epi32_mask(k, vec2, key);
-        auto tz = _tzcnt_u32(cmp | (1u << veclen)); // upper bound
-        lo += tz;
-        TERARK_ASSERT_LT(Rank1, rank_cache[lo].lev1);
-    }
+    __mmask8 k = _bzhi_u32(-1, veclen);
+    __m512i vec1 = _mm512_maskz_loadu_epi64(k, &rank_cache[lo]);
+    __m256i vec2 = _mm512_cvtepi64_epi32(vec1); // keep lev1(low32)
+    __m256i key = _mm256_set1_epi32(Rank1);
+    __mmask8 cmp = _mm256_mask_cmpgt_epi32_mask(k, vec2, key);
+    auto tz = _tzcnt_u32(cmp | (1u << veclen)); // upper bound
+    lo += tz;
+    TERARK_ASSERT_LT(Rank1, rank_cache[lo].lev1);
   #else
     while (lo < hi) {
         size_t mid = (lo + hi) / 2;
--- src/terark/succinct/rank_select_se_256.hpp
+++ src/terark/succinct/rank_select_se_256.hpp
@@ -116,19 +116,17 @@ noexcept {
         else
             hi = mid;
     }
-    {
-        __m256i vec0 = _mm256_add_epi32(_mm256_set1_epi32(lo), _mm256_set_epi32(7,6,5,4,3,2,1,0));
-        vec0 = _mm256_sllv_epi32(vec0, _mm256_set1_epi32(LineShift));
-        __mmask8 k = _bzhi_u32(-1, sublen);
-        __m512i vec1 = _mm512_maskz_loadu_epi64(k, &rankCache[lo]);
-        __m256i vec2 = _mm512_cvtepi64_epi32(vec1); // keep lev1(low32)
-        __m256i vec3 = _mm256_sub_epi32(vec0, vec2);
-        __m256i key = _mm256_set1_epi32(Rank0);
-        __mmask8 cmp = _mm256_mask_cmpgt_epi32_mask(k, vec3, key);
-        auto tz = _tzcnt_u32(cmp | (1u << sublen)); // upper bound
-        lo += tz;
-        TERARK_ASSERT_LT(Rank0, LineBits * lo - rankCache[lo].lev1);
-    }
+    __m256i vec0 = _mm256_add_epi32(_mm256_set1_epi32(lo), _mm256_set_epi32(7,6,5,4,3,2,1,0));
+    vec0 = _mm256_sllv_epi32(vec0, _mm256_set1_epi32(LineShift));
+    __mmask8 k = _bzhi_u32(-1, sublen);
+    __m512i vec1 = _mm512_maskz_loadu_epi64(k, &rankCache[lo]);
+    __m256i vec2 = _mm512_cvtepi64_epi32(vec1); // keep lev1(low32)
+    __m256i vec3 = _mm256_sub_epi32(vec0, vec2);
+    __m256i key = _mm256_set1_epi32(Rank0);
+    __mmask8 cmp = _mm256_mask_cmpgt_epi32_mask(k, vec3, key);
+    auto tz = _tzcnt_u32(cmp | (1u << sublen)); // upper bound
+    lo += tz;
+    TERARK_ASSERT_LT(Rank0, LineBits * lo - rankCache[lo].lev1);
   #else
     if (hi - lo < 32) {
         while (LineBits * lo - rankCache[lo].lev1 <= Rank0) lo++;
@@ -204,16 +202,14 @@ noexcept {
         else
             hi = mid;
     }
-    {
-        __mmask8 k = _bzhi_u32(-1, sublen);
-        __m512i vec1 = _mm512_maskz_loadu_epi64(k, &rankCache[lo]);
-        __m256i vec2 = _mm512_cvtepi64_epi32(vec1); // keep lev1(low32)
-        __m256i key = _mm256_set1_epi32(Rank1);
-        __mmask8 cmp = _mm256_mask_cmpgt_epi32_mask(k, vec2, key);
-        auto tz = _tzcnt_u32(cmp | (1u << sublen)); // upper bound
-        lo += tz;
-        TERARK_ASSERT_LT(Rank1, rankCache[lo].lev1);
-    }
+    __mmask8 k = _bzhi_u32(-1, sublen);
+    __m512i vec1 = _mm512_maskz_loadu_epi64(k, &rankCache[lo]);
+    __m256i vec2 = _mm512_cvtepi64_epi32(vec1); // keep lev1(low32)
+    __m256i key = _mm256_set1_epi32(Rank1);
+    __mmask8 cmp = _mm256_mask_cmpgt_epi32_mask(k, vec2, key);
+    auto tz = _tzcnt_u32(cmp | (1u << sublen)); // upper bound
+    lo += tz;
+    TERARK_ASSERT_LT(Rank1, rankCache[lo].lev1);
   #else
     if (hi - lo < 32) {
         while (rankCache[lo].lev1 <= Rank1) lo++;
