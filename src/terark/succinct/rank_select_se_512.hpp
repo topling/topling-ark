f655e7ffb3e925c9abb1f9ee79414a8ce7c5ee6b
rockeet <rockeet@gmail.com>
2025-10-14 18:55:07 +0800

    rank_select_se_512.hpp: select[01]_upper_bound_line(): remove param `bits`

--- src/terark/succinct/rank_select_se_512.hpp
+++ src/terark/succinct/rank_select_se_512.hpp
@@ -61,8 +61,8 @@ protected:
     size_t     m_max_rank1;
     size_t select0_upper_bound_line_safe(size_t id) const noexcept;
     size_t select1_upper_bound_line_safe(size_t id) const noexcept;
-    static inline size_t select0_upper_bound_line(const bm_uint_t* bits, const index_t* sel0, const RankCache512*, size_t id) noexcept;
-    static inline size_t select1_upper_bound_line(const bm_uint_t* bits, const index_t* sel1, const RankCache512*, size_t id) noexcept;
+    static inline size_t select0_upper_bound_line(const index_t* sel0, const RankCache512*, size_t id) noexcept;
+    static inline size_t select1_upper_bound_line(const index_t* sel1, const RankCache512*, size_t id) noexcept;
 public:
     const RankCache512* get_rank_cache() const { return m_rank_cache; }
     const index_t* get_sel0_cache() const { return m_sel0_cache; }
@@ -119,7 +119,7 @@ fast_rank1(const bm_uint_t* bits, const RankCache512* rankCache, size_t bitpos) 
 template<class rank_cache_base_t>
 inline size_t rank_select_se_512_tpl<rank_cache_base_t>::
 select0_upper_bound_line
-(const bm_uint_t* bits, const index_t* sel0, const RankCache512* rankCache, size_t Rank0)
+(const index_t* sel0, const RankCache512* rankCache, size_t Rank0)
 noexcept {
     size_t lo = sel0[Rank0 / LineBits];
     size_t hi = sel0[Rank0 / LineBits + 1];
@@ -180,7 +180,7 @@ template<class rank_cache_base_t>
 inline size_t rank_select_se_512_tpl<rank_cache_base_t>::fast_select0
 (const bm_uint_t* bits, const index_t* sel0, const RankCache512* rankCache, size_t Rank0)
 noexcept {
-    size_t lo = select0_upper_bound_line(bits, sel0, rankCache, Rank0);
+    size_t lo = select0_upper_bound_line(sel0, rankCache, Rank0);
     assert(Rank0 < LineBits * lo - rankCache[lo].base);
     const uint64_t* pBit64 = (const uint64_t*)(bits + LineWords * (lo-1));
     _mm_prefetch((const char*)(pBit64+0), _MM_HINT_T0);
@@ -237,7 +237,7 @@ noexcept {
 template<class rank_cache_base_t>
 inline size_t rank_select_se_512_tpl<rank_cache_base_t>::
 select1_upper_bound_line
-(const bm_uint_t* bits, const index_t* sel1, const RankCache512* rankCache, size_t Rank1)
+(const index_t* sel1, const RankCache512* rankCache, size_t Rank1)
 noexcept {
     size_t lo = sel1[Rank1 / LineBits];
     size_t hi = sel1[Rank1 / LineBits + 1];
@@ -261,7 +261,7 @@ template<class rank_cache_base_t>
 inline size_t rank_select_se_512_tpl<rank_cache_base_t>::fast_select1
 (const bm_uint_t* bits, const index_t* sel1, const RankCache512* rankCache, size_t Rank1)
 noexcept {
-    size_t lo = select1_upper_bound_line(bits, sel1, rankCache, Rank1);
+    size_t lo = select1_upper_bound_line(sel1, rankCache, Rank1);
     assert(Rank1 < rankCache[lo].base);
     const uint64_t* pBit64 = (const uint64_t*)(bits + LineWords * (lo-1));
     _mm_prefetch((const char*)(pBit64+0), _MM_HINT_T0);
